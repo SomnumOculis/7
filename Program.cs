39eb1bd99bec8436044b78159b850a5843a8dba2
Somnum Oculis <konikovskaatanusa@gmail.com>
2023-06-07 21:49:24 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace blokcxema.zadacha_7.sem2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, n, i;
+            try
+            {
+                Console.WriteLine("Введите количество элементов массива ");
+                n = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine(" Повторить ввод данных? ( 1-yes, 0-no ): ");
+                a = Convert.ToInt32(Console.ReadLine());
+                if (a == 1) { Main(); }
+                return;
+            }
+            Random rand = new Random();
+            int[] array = new int[n];
+            for (i = 0; i < n; i++)
+            {
+                array[i] = rand.Next(-2, 4);
+                Console.Write("{0,3}", i);
+            }
+            Console.WriteLine();
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("{0,3}", array[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            for (i = 0; i < array.Length; i++)
+            {
+                if (array[i] == 0)
+                {
+                    continue;
+                }
+                Console.Write("{0,3}", array[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine(" Повторить ввод данных? ( 1-yes, 0-no ): ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a == 1) { Main(); }
+            return;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("blokcxema.zadacha№7.sem2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("blokcxema.zadacha№7.sem2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("bd5b2eca-ffed-47f2-9061-b8ba4ce8632c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
